05799ac5f6667e0403e6995cf67dc6e675ee44fa
Henrique Frank <61198260+henrique-frank@users.noreply.github.com>
2024-01-21 11:17:38 -0300

    Complete CRUD app NodeJS Express

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 const cursos = ['NodeJS', 'JavaScript', 'ReactNative']
 
-// localhost:3000/curso
-server.get('/curso/:index', (req, res) =>{
+server.get('/cursos', (req, res)=> {
+    return res.json(cursos)
+})
+
+// localhost:3000/cursos
+server.get('/cursos/:index', (req, res) =>{
     const { index } = req.params
-    return res.json({curso: `Código da Aprovação: Aula ${cursos[index]}`})
+    return res.json({curso: `Código da Aprovação: aula ${cursos[index]}`})
+})
+
+server.post('/cursos', (req, res)=>{
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos)
+})
+
+server.put('/cursos/:index', (req, res)=>{
+    const {index} = req.params;
+    const {name} = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+})
+
+server.delete('/cursos/:index', (req, res)=>{
+    const {index} = req.params;
+
+    cursos.splice(index, 1);
+
+    return res.json(cursos);
 })
 
 server.listen(3000);
