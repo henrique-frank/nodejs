cc3f267325577b549af95b70dde6979e438d01f0
Henrique Frank <henrique_frank@hotmail.com>
2024-01-21 10:15:18 -0300

    adding routing params

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const server = express();
 
 // localhost:3000/curso
-server.get('/curso', (req, res) =>{
-    return res.json({curso: "Código da Aprovação!"})
+server.get('/curso/:id', (req, res) =>{
+    const id = req.params.id
+    return res.json({curso: `Código da Aprovação: ID ${id}`})
 })
 
 server.listen(3000);
