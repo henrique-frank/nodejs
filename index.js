d5cdde6fc6b8e748687a9a0099722fadd1cb9e4f
Henrique Frank <henrique_frank@hotmail.com>
2024-01-21 10:27:35 -0300

    add params and index for classes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const server = express();
 
+const cursos = ['NodeJS', 'JavaScript', 'ReactNative']
+
 // localhost:3000/curso
-server.get('/curso/:id', (req, res) =>{
-    const id = req.params.id
-    return res.json({curso: `Código da Aprovação: ID ${id}`})
+server.get('/curso/:index', (req, res) =>{
+    const { index } = req.params
+    return res.json({curso: `Código da Aprovação: Aula ${cursos[index]}`})
 })
 
 server.listen(3000);
