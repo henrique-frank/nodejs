70a3c776489e947e952b5394f6e92766c0ae0704
Henrique Frank <61198260+henrique-frank@users.noreply.github.com>
2024-01-21 19:15:57 -0300

    Adding middlewares to check if request is correct

--- index.js
+++ index.js
@@ -6,24 +6,49 @@ server.use(express.json());
 
 const cursos = ['NodeJS', 'JavaScript', 'ReactNative']
 
+server.use((req,res,next)=>{
+    console.log(`url chamada: ${req.url}`);
+    return next();
+});
+
+// Middleware para evitar erros no request
+function checkCurso(req, res, next){
+    if (!req.body.name){
+        return res.status(400).json({ error: "Nome do curso é obrigatório"});
+    }
+
+    return next();
+}
+
+function checkIndex(req, res, next){
+    const curso = cursos[req.params.index]
+    if (!curso){
+        return res.status(400).json({ error: "O curso não existe"});
+    }
+
+    req.curso = curso;
+
+    return next();
+}
+
+
 server.get('/cursos', (req, res)=> {
     return res.json(cursos)
-})
+});
 
 // localhost:3000/cursos
-server.get('/cursos/:index', (req, res) =>{
-    const { index } = req.params
-    return res.json({curso: `Código da Aprovação: aula ${cursos[index]}`})
-})
+server.get('/cursos/:index', checkIndex, (req, res) =>{
+    return res.json({curso: `Código da Aprovação: aula ${req.curso}`})
+});
 
-server.post('/cursos', (req, res)=>{
+server.post('/cursos', checkCurso, (req, res)=>{
     const { name } = req.body;
     cursos.push(name);
 
     return res.json(cursos)
 })
 
-server.put('/cursos/:index', (req, res)=>{
+server.put('/cursos/:index', checkCurso, checkIndex, (req, res)=>{
     const {index} = req.params;
     const {name} = req.body;
 
@@ -32,7 +57,7 @@ server.put('/cursos/:index', (req, res)=>{
     return res.json(cursos);
 })
 
-server.delete('/cursos/:index', (req, res)=>{
+server.delete('/cursos/:index', checkIndex, (req, res)=>{
     const {index} = req.params;
 
     cursos.splice(index, 1);
